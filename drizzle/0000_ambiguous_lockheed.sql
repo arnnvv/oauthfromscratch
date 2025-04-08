9d67eade6d8f7c21a73efc7cd60417e6614e16a5
arnnvv <arnav.900@protonmail.com>
2025-04-08 22:16:55 +0530

    up

---
+++ drizzle/0000_ambiguous_lockheed.sql
@@ -0,0 +1,20 @@
+CREATE TABLE "oauthtry_sessions" (
+	"id" text PRIMARY KEY NOT NULL,
+	"user_id" integer NOT NULL,
+	"expires_at" timestamp with time zone NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "oauthtry_users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"google_id" text NOT NULL,
+	"email" varchar NOT NULL,
+	"name" text NOT NULL,
+	"picture" text NOT NULL,
+	CONSTRAINT "oauthtry_users_google_id_unique" UNIQUE("google_id"),
+	CONSTRAINT "oauthtry_users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+ALTER TABLE "oauthtry_sessions" ADD CONSTRAINT "oauthtry_sessions_user_id_oauthtry_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."oauthtry_users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "session_user_id_idx" ON "oauthtry_sessions" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "google_id_idx" ON "oauthtry_users" USING btree ("google_id");--> statement-breakpoint
+CREATE INDEX "email_idx" ON "oauthtry_users" USING btree ("email");
